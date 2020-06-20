e159dcc745f7f1fd0eccaaac1c6ba2181b108c24
Casey Cook <cook.caseya@gmail.com>
2020-06-20 15:29:47 -0400

    updated tech icons to reflect mobile dev

--- public/app.js
+++ public/app.js
@@ -11,38 +11,39 @@ window.onscroll = function () {
 };
 
 //Snagging from form
-document.getElementById("send").addEventListener("click", validateForm);
-      function validateForm() {
-        //gets the name
-        let name = document.getElementById("contact-name").value;
-        //gets the email
-        let email = document.getElementById("contact-email").value;
-        //gets the message
-        let message = document.getElementById("contact-message").value;
+document.getElementById('send').addEventListener('click', validateForm);
+function validateForm() {
+	//gets the name
+	let name = document.getElementById('contact-name').value;
+	//gets the email
+	let email = document.getElementById('contact-email').value;
+	//gets the message
+	let message = document.getElementById('contact-message').value;
 
-        //checks if all fields have been filled before sending message.
-        if (name.trim() == "" || email.trim() == "" || message.trim() == "") {
-          alert("all fields must be filled");
-        } else {
-          sendMessage(name, email, message);
-        }
-      }
+	//checks if all fields have been filled before sending message.
+	if (name.trim() == '' || email.trim() == '' || message.trim() == '') {
+		alert('all fields must be filled');
+	} else {
+		sendMessage(name, email, message);
+	}
+}
 
-      //sends information to firebase
-      function sendMessage(name, email, message) {
-        console.log('sending message');
-        //sends the name, email and message by passing them as url parameters
-        var myWindow = window.open(
-          "https://us-central1-caseycookdev.cloudfunctions.net/sendMail?name=" +
-          name +
-          "&email=" +
-          email +
-          "&message=" +
-          message +
-          "");
+//sends information to firebase
+function sendMessage(name, email, message) {
+	console.log('sending message');
+	//sends the name, email and message by passing them as url parameters
+	var myWindow = window.open(
+		'https://us-central1-caseycookdev.cloudfunctions.net/sendMail?name=' +
+			name +
+			'&email=' +
+			email +
+			'&message=' +
+			message +
+			''
+	);
 
-          //manual form reset
-          document.getElementById("contact-name").value = '';
-          document.getElementById("contact-email").value = '';
-          document.getElementById("contact-message").value = '';
-      }
+	//manual form reset
+	document.getElementById('contact-name').value = '';
+	document.getElementById('contact-email').value = '';
+	document.getElementById('contact-message').value = '';
+}
